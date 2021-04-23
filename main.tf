b2a168040ba1f760cbc9ba2eede3021a02c8d352
Ivan Tsyplenkov <vankas2010@yandex.ru>
2021-04-23 16:53:25 +0300

    Run the default ubuntu web-server and add the security group

--- main.tf
+++ main.tf
@@ -3,10 +3,31 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
+  # AMI id take from WEB console while try to create instance.
   ami           = "ami-013f17f36f8b1fefb"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
+  # Create simple web server with busybox default tool
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hellow, world!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  # Give a name to server.
   tags = {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Listen All IP addresses on port 8080
   }
 } 
