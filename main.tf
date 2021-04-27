4fa9e5da125b661c24025cb7ba48f81d873e3ce8
Ivan Tsyplenkov <vankas2010@yandex.ru>
2021-04-27 13:40:27 +0300

    Study deploy Auto Scalling Group in AWS

--- main.tf
+++ main.tf
@@ -1,23 +1,41 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
+#resource "aws_instance" "example" {
+#  ami = "ami-01e7ca2ef94a0ae86"
+#  instance_type = "t2.micro"
+#  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  # Create simple web server with busybox default tool
+#  user_data = <<-EOF
+ #             #!/bin/bash
+ #             echo "Hellow, world!" > index.html
+ ##             nohup busybox httpd -f -p ${var.server_port} &
+ #             EOF
+
+  # Give a name to server.
+#  tags = {
+#  Name = "terraform-example"
+#  }
+#}
+
+resource "aws_launch_configuration" "example" {
   # AMI id take from WEB console while try to create instance.
-  ami           = "ami-013f17f36f8b1fefb"
+  image_id           = "ami-01e7ca2ef94a0ae86"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
 
   # Create simple web server with busybox default tool
   user_data = <<-EOF
               #!/bin/bash
               echo "Hellow, world!" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
-  # Give a name to server.
-  tags = {
-    Name = "terraform-example"
+  # Required when using a launch configuraion with an auto scaling group.
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -25,9 +43,48 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Listen All IP addresses on port 8080
   }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+  min_size = 2 
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests."
+  type        = number
+  default     = 8080
 } 
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server."
+}
+
+resource "aws_lb" "example" {
+  name        = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets     = data.aws_subnet_ids.default.ids
+}
