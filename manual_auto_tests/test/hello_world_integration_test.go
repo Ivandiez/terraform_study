49523019d7fef97e8306ba4092119e3b62a31e4b
Ivan Tsyplenkov <vankas2010@yandex.ru>
2021-05-16 22:42:00 +0300

    study integration and end-to-end test

--- manual_auto_tests/test/hello_world_integration_test.go
+++ manual_auto_tests/test/hello_world_integration_test.go
@@ -4,14 +4,15 @@ import (
   "strings"
   "time"
   "fmt"
-  "github.com/gruntwork-io/terratest/modules/http-helper"
+  http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
   "github.com/gruntwork-io/terratest/modules/random"
   "github.com/gruntwork-io/terratest/modules/terraform"
+  "github.com/gruntwork-io/terratest/modules/test-structure"
   "testing"
 )
 
-const dbDirStage = "../live/stage/data-stores/mysql"
-const appDirStage = "../live/stage/services/hello-world-app"
+const dbDirStage = "/home/ivan/study_terraform/up_first_aws_server/manual_auto_tests/live/stage/data-stores/mysql"
+const appDirStage = "/home/ivan/study_terraform/up_first_aws_server/manual_auto_tests/live/stage/services/hello-world-app"
 
 func TestHelloWorldAppStage(t *testing.T) {
   t.Parallel()
@@ -33,12 +34,12 @@ func TestHelloWorldAppStage(t *testing.T) {
 func createDbOpts(t *testing.T, terraformDir string) *terraform.Options {
   uniqueId := random.UniqueId()
 
-  bucketForTesting := "us-east-2-test-bucket-ivan"
+  bucketForTesting := "us-east-2-ivandock-el"
   bucketRegionForTesting := "us-east-2"
   dbStateKey := fmt.Sprintf("%s/%s/terraform.tfstate", t.Name(), uniqueId)
 
   return &terraform.Options{
-    TerraformDir := terraformDir,
+    TerraformDir: terraformDir,
 
     Vars: map[string]interface{}{
       "db_name": fmt.Sprintf("test%s", uniqueId),
@@ -66,12 +67,20 @@ func createHelloOpts(
           "db_remote_state_key": dbOpts.BackendConfig["key"],
           "environment": dbOpts.Vars["db_name"],
         },
+
+        // Retry up to 3 times, with 5 seconds between retries,
+        // on known errors
+        MaxRetries: 3,
+        TimeBetweenRetries: 5 * time.Second,
+        RetryableTerraformErrors: map[string]string{
+            "RequestError: send request failed": "Throttling issue?",
+        },
       }
 }
 
 func validateHelloApp(t *testing.T, helloOpts *terraform.Options) {
   albDnsName := terraform.OutputRequired(t, helloOpts, "alb_dns_name")
-  url := fmt.Sprintf("http://%s", string.Trim(albDnsName, "\""))
+  url := fmt.Sprintf("http://%s", strings.Trim(albDnsName, "\""))
 
   maxRetries := 10
   timeBetweenRetries := 10 * time.Second
@@ -86,4 +95,99 @@ func validateHelloApp(t *testing.T, helloOpts *terraform.Options) {
 				strings.Contains(body, "Hello, World")
 		},
 	)
+}
+
+func TestHelloWorldAppStageWithStages(t *testing.T) {
+  t.Parallel()
+
+  stage := test_structure.RunTestStage
+
+  // Deploy the MySQL DB
+  defer stage(t, "teardown_db", func() { teardownDb(t, dbDirStage) })
+  stage(t, "deploy_db", func() { deployDb(t, dbDirStage) })
+
+  // Deploy the hello-world-app
+  defer stage(t, "teardown_app", func() { teardownApp(t, appDirStage) })
+  stage(t, "deploy_app", func() { deployApp(t, dbDirStage, appDirStage) })
+
+  // Validate the hello-world-app works
+  stage(t, "validate_app", func() { validateApp(t, appDirStage) })
+
+  // Redeploy the hello-world-app
+	stage(t, "redeploy_app", func() { redeployApp(t, appDirStage) })
+}
+
+func deployDb(t *testing.T, dbDir string) {
+  dbOpts := createDbOpts(t, dbDir)
+
+  // Save data to disk so that other test stages executed at a later 
+  // time can read data back in
+  test_structure.SaveTerraformOptions(t, dbDir, dbOpts)
+
+  terraform.InitAndApply(t, dbOpts)
+}
+
+func teardownDb(t *testing.T, dbDir string) {
+  dbOpts := test_structure.LoadTerraformOptions(t, dbDir)
+  defer terraform.Destroy(t, dbOpts)
+}
+
+func deployApp(t *testing.T, dbDir string, helloAppDir string) {
+  dbOpts := test_structure.LoadTerraformOptions(t, dbDir)
+  helloOpts := createHelloOpts(dbOpts, helloAppDir)
+
+  // Save data to disk so that other test stages executed at a later 
+  // time can read data back in
+  test_structure.SaveTerraformOptions(t, helloAppDir,helloOpts)
+
+  terraform.InitAndApply(t, helloOpts)
+}
+
+func teardownApp(t *testing.T, helloAppDir string) {
+  helloOpts := test_structure.LoadTerraformOptions(t, helloAppDir)
+  defer terraform.Destroy(t, helloOpts)
+}
+
+func validateApp(t *testing.T, helloAppDir string) {
+  helloOpts := test_structure.LoadTerraformOptions(t, helloAppDir)
+  validateHelloApp(t, helloOpts)
+}
+
+func redeployApp(t *testing.T, helloAppDir string) {
+  helloOpts := test_structure.LoadTerraformOptions(t, helloAppDir)
+
+  albDnsName := terraform.OutputRequired(t, helloOpts, "alb_dns_name")
+  url := fmt.Sprintf("http://%s", strings.Trim(albDnsName, "\""))
+
+  // Start checking every 1s that the app is responding with a 200 OK
+  stopChecking := make(chan bool, 1)
+  waitGroup, _ := http_helper.ContinuouslyCheckUrl(
+          t,
+          url,
+          stopChecking,
+          1*time.Second,
+  )
+
+  // Update the server text and redeploy
+  newServerText := "Hello, World, v2!"
+  helloOpts.Vars["server_text"] = newServerText
+  terraform.Apply(t, helloOpts)
+
+  // Make sure the new version deployed
+	maxRetries := 10
+	timeBetweenRetries := 10 * time.Second
+	http_helper.HttpGetWithRetryWithCustomValidation(
+		t,
+		url,
+		maxRetries,
+		timeBetweenRetries,
+		func(status int, body string) bool {
+			return status == 200 &&
+				strings.Contains(body, newServerText)
+		},
+	)
+
+  // Stop checking
+  stopChecking <- true
+  waitGroup.Wait()
 }
